821965620d16ac9f809ecb3d34bb801be33f7868
Dmitry Doubatovka <dmitry.doubatovka@rokolabs.com>
2019-04-01 16:55:17 +0400

    implemented Tiles tables viewer

---
+++ NESEmul.PatternTablesViewer/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿using System.Windows.Forms;
+using SkiaSharp.Views.Desktop;
+
+namespace NESEmul.PatternTablesViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._skControl = new SkiaSharp.Views.Desktop.SKControl();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.openROMFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _skControl
+            // 
+            this._skControl.Dock = System.Windows.Forms.DockStyle.Left;
+            this._skControl.Location = new System.Drawing.Point(0, 24);
+            this._skControl.Name = "_skControl";
+            this._skControl.Size = new System.Drawing.Size(1027, 438);
+            this._skControl.TabIndex = 0;
+            this._skControl.PaintSurface += new System.EventHandler<SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs>(this.OnSkControlOnPaint);
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenu});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(1424, 24);
+            this.menu.TabIndex = 2;
+            this.menu.Text = "menu";
+            // 
+            // fileMenu
+            // 
+            this.fileMenu.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFile});
+            this.fileMenu.Name = "fileMenu";
+            this.fileMenu.Size = new System.Drawing.Size(37, 20);
+            this.fileMenu.Text = "File";
+            // 
+            // openFile
+            // 
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(180, 22);
+            this.openFile.Text = "&Open";
+            this.openFile.Click += new System.EventHandler(this.openFileMenuItem_Click);
+            // 
+            // openROMFileDialog
+            // 
+            this.openROMFileDialog.DefaultExt = "rom";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1424, 462);
+            this.Controls.Add(this._skControl);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "Form1";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private SkiaSharp.Views.Desktop.SKControl _skControl;
+        private MenuStrip menu;
+        private ToolStripMenuItem fileMenu;
+        private ToolStripMenuItem openFile;
+        private OpenFileDialog openROMFileDialog;
+    }
+}
+
---
+++ NESEmul.PatternTablesViewer/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using NESEmul.Core;
+using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+
+namespace NESEmul.PatternTablesViewer
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<Tile> _tileList;
+        public Form1()
+        {
+            InitializeComponent();
+            _tileList = new List<Tile>(512);
+        }
+
+
+        private void OnSkControlOnPaint(object sender, SKPaintSurfaceEventArgs args)
+        {
+            if(!_tileList.Any())
+                return;
+            var stopWatch = Stopwatch.StartNew();
+            int cellSize = 3;
+            var surface = args.Surface;
+
+            SKCanvas canvas = surface.Canvas;
+            canvas.DrawColor(SKColors.White);
+            canvas.Clear(SKColors.White);
+            
+            using (SKPaint paint = new SKPaint())
+            {
+                paint.StrokeWidth = 3;
+                paint.Style = SKPaintStyle.Fill;
+
+                List<SKPoint> points1 = new List<SKPoint>(64 * 64);
+                List<SKPoint> points2 = new List<SKPoint>(64 * 64);
+                List<SKPoint> points3 = new List<SKPoint>(64 * 64);
+                for (int tileIndex = 0; tileIndex < _tileList.Count; tileIndex++)
+                {
+                    var tile = _tileList[tileIndex];
+                    int y = tileIndex / 16;
+                    int x = tileIndex - y * 16;
+
+                    TileColors[,] colors = tile.As8X8Array();
+                    for (int j = 0; j < colors.GetLength(0); j++)
+                    {
+                        for (int i = 0; i < colors.GetLength(1); i++)
+                        {
+                            var p = new SKPoint(8 * x * cellSize + i * cellSize, 8 * y * cellSize + j * cellSize);
+                            var tilePixelColor = colors[i, j];
+                            if(tilePixelColor == TileColors.FirstColor)
+                                points1.Add(p);
+                            else if(tilePixelColor == TileColors.SecondColor)
+                                points2.Add(p);
+                            else if(tilePixelColor == TileColors.ThirdColor)
+                                points3.Add(p);
+                        }
+                    }
+
+                }
+
+                paint.Color = SKColors.Red;
+                canvas.DrawPoints(SKPointMode.Points, points1.ToArray(), paint);
+                paint.Color = SKColors.Blue;
+                canvas.DrawPoints(SKPointMode.Points, points2.ToArray(), paint);
+                paint.Color = SKColors.Black;
+                canvas.DrawPoints(SKPointMode.Points, points3.ToArray(), paint);
+
+                stopWatch.Stop();
+                Debug.WriteLine(stopWatch.ElapsedMilliseconds);
+            }
+        }
+
+        private void openFileMenuItem_Click(object sender, EventArgs e)
+        {
+            var romImage = new ROMImage();
+            if (openROMFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using(var stream = openROMFileDialog.OpenFile())
+                    romImage.Load(stream);
+                if(romImage.VROMBanksNumber < 1)
+                    return;
+                byte[] vromBank = romImage.VROMBanks.Single();
+                _tileList.Clear();
+                for (int i = 0; i < vromBank.Length / 16; i++)
+                {
+                    _tileList.Add(new Tile(vromBank.Skip(i * 16).Take(16).ToArray()));
+                }
+                _skControl.Invalidate();
+            }
+
+            
+        }
+    }
+}

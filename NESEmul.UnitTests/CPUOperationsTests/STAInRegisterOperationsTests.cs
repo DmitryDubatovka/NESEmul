54cf3bb448b50e2830bd2655adce5f59fcc98f9f
Dmitry Dubatovka <dubatovkapublic@mail.ru>
2019-04-30 22:54:34 +0400

    fixed some tests

--- NESEmul.UnitTests/CPUOperationsTests/STAInRegisterOperationsTests.cs
+++ NESEmul.UnitTests/CPUOperationsTests/STAInRegisterOperationsTests.cs
@@ -39,91 +39,111 @@ namespace NESEmul.UnitTests.CPUOperationTests
         [Test]
         public void STAAbsXOperationTest()
         {
-            InitAbsXMode(OpCodes.STAAbsX, 0x80);
+            CPU.Accumulator = 0x20;
+            InitAbsXMode(OpCodes.STAAbsX, 0x0, 1, 0, 0x01, 0x02);
+            Assert.That(Memory.LoadByteFromMemory(0x0103), Is.EqualTo(0x0));
             CPU.Do();
-            Assert.That(CPU.Accumulator, Is.EqualTo(0x80));
+            Assert.That(Memory.LoadByteFromMemory(0x0103), Is.EqualTo(0x20));
         }
-        
+
         [Test]
         public void STAAbsYOperationTest()
         {
-            InitAbsYMode(OpCodes.STAAbsY, 0x80);
+            CPU.Accumulator = 0x20;
+            InitAbsYMode(OpCodes.STAAbsY, 0x0, 1, 0, 0x01, 0x02);
+            Assert.That(Memory.LoadByteFromMemory(0x0103), Is.EqualTo(0x0));
             CPU.Do();
-            Assert.That(CPU.Accumulator, Is.EqualTo(0x80));
+            Assert.That(Memory.LoadByteFromMemory(0x0103), Is.EqualTo(0x20));
         }
         
         [Test]
         public void STAIndXOperationTest()
         {
+            CPU.Accumulator = 0x20;
             Memory.StoreByteInMemory(0, (byte)OpCodes.STAIndX);
             CPU.IndexRegisterX = 0xFF;
             Memory.StoreByteInMemory(0x1, 0x10);
             Memory.StoreByteInMemory(0x0F, 0x02);
             Memory.StoreByteInMemory(0x10, 0x04);
-            Memory.StoreByteInMemory(0x0402, 0x80);
+            Assert.That(Memory.LoadByteFromMemory(0x0402), Is.EqualTo(0x0));
             CPU.Do();
-            Assert.That(CPU.Accumulator, Is.EqualTo(0x80));
+            Assert.That(Memory.LoadByteFromMemory(0x0402), Is.EqualTo(0x20));
         }
         
         [Test]
         public void STAIndYOperationTest()
         {
+            CPU.Accumulator = 0x20;
             Memory.StoreByteInMemory(0, (byte) OpCodes.STAIndY);
             CPU.IndexRegisterY = 0xFF;
             Memory.StoreByteInMemory(0x1, 0x10);
             Memory.StoreByteInMemory(0x10, 0x04);
             Memory.StoreByteInMemory(0x11, 0x02);
-            Memory.StoreByteInMemory(0x303, 0x80);
+            Assert.That(Memory.LoadByteFromMemory(0x0303), Is.EqualTo(0x0));
             CPU.Do();
-            Assert.That(CPU.Accumulator, Is.EqualTo(0x80));
+            Assert.That(Memory.LoadByteFromMemory(0x0303), Is.EqualTo(0x20));
         }
 
         [Test]
         public void STXZPOperationTest()
         {
-            InitZPMode(OpCodes.STXZP, 0x81);
+            CPU.IndexRegisterX = 0x81;
+            InitZPMode(OpCodes.STXZP, 0x0, 0, 0x10);
+            Assert.That(Memory.LoadByteFromMemory(0x10), Is.EqualTo(0x0));
             CPU.Do();
-            Assert.That(CPU.IndexRegisterX, Is.EqualTo(0x81));
+            Assert.That(Memory.LoadByteFromMemory(0x10), Is.EqualTo(0x81));
         }
         
         [Test]
         public void STYZPOperationTest()
         {
-            InitZPMode(OpCodes.STYZP, 0x82);
+            CPU.IndexRegisterY = 0x82;
+            InitZPMode(OpCodes.STYZP, 0x0, 0, 0x10);
+            Assert.That(Memory.LoadByteFromMemory(0x10), Is.EqualTo(0x0));
             CPU.Do();
-            Assert.That(CPU.IndexRegisterY, Is.EqualTo(0x82));
+            Assert.That(Memory.LoadByteFromMemory(0x10), Is.EqualTo(0x82));
         }
 
         [Test]
         public void STXAbsOperationTest()
         {
-            InitAbsMode(OpCodes.STXAbs, 0x30);
+            CPU.IndexRegisterX = 0x30;
+            InitAbsMode(OpCodes.STXAbs, 0, 0, 1, 2);
+            Assert.That(Memory.LoadByteFromMemory(0x0102), Is.EqualTo(0x0));
             CPU.Do();
-            Assert.That(CPU.IndexRegisterX, Is.EqualTo(0x30));
+            Assert.That(Memory.LoadByteFromMemory(0x0102), Is.EqualTo(0x30));
         }
         
         [Test]
         public void STYAbsOperationTest()
         {
-            InitAbsMode(OpCodes.STYAbs, 0x32);
+            CPU.IndexRegisterY = 0x32;
+            InitAbsMode(OpCodes.STYAbs, 0, 0, 1, 2);
+            Assert.That(Memory.LoadByteFromMemory(0x0102), Is.EqualTo(0x0));
             CPU.Do();
-            Assert.That(CPU.IndexRegisterY, Is.EqualTo(0x32));
+            Assert.That(Memory.LoadByteFromMemory(0x0102), Is.EqualTo(0x32));
         }
 
         [Test]
         public void STXZPYOperationTest()
         {
-            InitZPYMode(OpCodes.STXZPY, 0x72);
+            CPU.IndexRegisterX = 0x72;
+            InitZPYMode(OpCodes.STXZPY, 0x0, 0x1, 0, 0xF);
+            Assert.That(Memory.LoadByteFromMemory(0x10), Is.EqualTo(0x0));
             CPU.Do();
-            Assert.That(CPU.IndexRegisterX, Is.EqualTo(0x72));
+            Assert.That(Memory.LoadByteFromMemory(0x10), Is.EqualTo(0x72));
+
+           
         }
         
         [Test]
         public void STYZPXOperationTest()
         {
-            InitZPXMode(OpCodes.STYZPX, 0x70);
+            CPU.IndexRegisterY = 0x70;
+            InitZPXMode(OpCodes.STYZPX, 0x0, 0x1, 0, 0xF);
+            Assert.That(Memory.LoadByteFromMemory(0x10), Is.EqualTo(0x0));
             CPU.Do();
-            Assert.That(CPU.IndexRegisterY, Is.EqualTo(0x70));
+            Assert.That(Memory.LoadByteFromMemory(0x10), Is.EqualTo(0x70));
         }
     }
 }
